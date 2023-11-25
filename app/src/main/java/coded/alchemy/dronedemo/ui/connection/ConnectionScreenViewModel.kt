a489f26091d7d6299644af9132b0c0ed7725cc08
Taji Abdullah <tajiabdullah1@gmail.com>
2023-11-25 13:39:58 -0500

    Move telemetry code to ControlScreenViewModel.

--- app/src/main/java/coded/alchemy/dronedemo/ui/connection/ConnectionScreenViewModel.kt
+++ app/src/main/java/coded/alchemy/dronedemo/ui/connection/ConnectionScreenViewModel.kt
@@ -17,13 +17,9 @@ class ConnectionScreenViewModel(
     private val droneRepository: DroneRepository,
     private val serverRepository: ServerRepository
 ) : ViewModel() {
-
     private val TAG = this.javaClass.simpleName
-    private val disposables = CompositeDisposable()
-
     private val _isConnected = MutableStateFlow<Boolean?>(false)
     val isConnected: StateFlow<Boolean?> get() = _isConnected
-
 
     fun connect() {
         Log.d(TAG, "connect: ")
@@ -32,33 +28,10 @@ class ConnectionScreenViewModel(
                 val port = serverRepository.mavServer().run()
                 droneRepository.drone = System(serverRepository.host, port)
                 _isConnected.emit(true)
-
-                disposables.add(
-                    droneRepository.drone.telemetry.flightMode.distinctUntilChanged()
-                        .subscribe { flightMode: Telemetry.FlightMode ->
-                            Log.d(TAG, "flight mode: $flightMode")
-                        }
-                )
-                disposables.add(
-                    droneRepository.drone.telemetry.armed.distinctUntilChanged()
-                        .subscribe { armed: Boolean ->
-                            Log.d(TAG, "armed: $armed")
-                        }
-                )
-                disposables.add(
-                    droneRepository.drone.telemetry.position
-                        .subscribe { position: Telemetry.Position ->
-//                        val latLng =
-//                            LatLng(position.latitudeDeg, position.longitudeDeg)
-//                        viewModel.currentPositionLiveData.postValue(latLng)
-                        })
-
-
             } catch (exception: Exception) {
                 _isConnected.emit(false)
                 Log.e(TAG, exception.toString())
             }
         }
-
     }
 }
--- app/src/main/java/coded/alchemy/dronedemo/ui/control/ControlScreenViewModel.kt
+++ app/src/main/java/coded/alchemy/dronedemo/ui/control/ControlScreenViewModel.kt
@@ -4,15 +4,51 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coded.alchemy.dronedemo.data.DroneRepository
+import io.mavsdk.telemetry.Telemetry
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ControlScreenViewModel(private val droneRepository: DroneRepository) : ViewModel() {
     private val TAG = this.javaClass.simpleName
     private val drone = droneRepository.drone
+    private val disposables = CompositeDisposable()
+
+    init {
+        getTelemetryData()
+    }
+
+    private fun getTelemetryData() {
+        Log.d(TAG, "getTelemetryData: ")
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                disposables.add(
+                    droneRepository.drone.telemetry.flightMode.distinctUntilChanged()
+                        .subscribe { flightMode: Telemetry.FlightMode ->
+                            Log.d(TAG, "flight mode: $flightMode")
+                        }
+                )
+                disposables.add(
+                    droneRepository.drone.telemetry.armed.distinctUntilChanged()
+                        .subscribe { armed: Boolean ->
+                            Log.d(TAG, "armed: $armed")
+                        }
+                )
+                disposables.add(
+                    droneRepository.drone.telemetry.position
+                        .subscribe { position: Telemetry.Position ->
+    //                        val latLng =
+    //                            LatLng(position.latitudeDeg, position.longitudeDeg)
+    //                        viewModel.currentPositionLiveData.postValue(latLng)
+                        })
+            } catch (e: Exception) {
+                Log.e(TAG, "getTelemetryData: $e")
+            }
+        }
+    }
 
     fun takeoff() {
-        Log.e(TAG, "takeoff: ")
+        Log.d(TAG, "takeoff: ")
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 drone.action.arm().andThen(drone.action.takeoff()).subscribe()
